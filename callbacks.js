63ec6b44d77ab06a7748df0872cd9654813cc0f3
DinaTaklit <ez_taklit@esi.dz>
2021-01-25 17:02:05 +0100

    callback.js

---
+++ callbacks.js
@@ -0,0 +1,24 @@
+// ########################################
+// # Callbacks and Higher-Order Functions #
+// ########################################
+
+console.log("===============Challenge 1===================");
+// Challenge 1
+// Create a function addTwo that accepts one input and adds 2 to it.
+function addTwo(num) {
+    return num + 2
+}
+console.log(addTwo(3));
+console.log(addTwo(10));
+console.log("===============/Challenge 1===================");
+
+
+console.log("===============Challenge 2===================");
+// Challenge 2
+// Create a function addS that accepts one input and adds an "s" to it.
+function addS(word) {
+  return word + 's'
+}
+console.log(addS('pizza'));
+console.log(addS('bagel'));
+console.log("===============/Challenge 2===================");
